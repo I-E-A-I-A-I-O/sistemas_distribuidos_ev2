72ce49bc959720b000115e4e8974beab70a6ba12
I-E-A-I-A-I-O <jesusdavid268@outlook.com>
2022-04-04 00:47:17 -0400

    copy changes to desktop bff

--- desktop-bff/dev/controllers/auth.controller.ts
+++ desktop-bff/dev/controllers/auth.controller.ts
@@ -1,13 +1,34 @@
 import { Request, Response } from 'express'
 import { Credentials } from '../helpers/types/credentials-schema'
+import { log } from '../helpers/logstash'
 
 export const register = async (request: Request, reply: Response) => {
     try {
-        const asd = await Credentials.parseAsync(request.body)
-        reply.status(200).json(asd)
+        const cr = await Credentials.parseAsync(request.body)
+        const response = await fetch(`http://127.0.0.1:8100${request.url}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(cr)
+        })
+        log('info', 'response-received', {
+            status: response.status,
+            path: request.url,
+            method: request.method,
+            ip: request.ip,
+            ua: request.headers['user-agent'] || null
+        })
+        const reBody = await response.json()
+        reply.status(response.status).json(reBody)
     } catch(err) {
-        reply.status(500).send('XD')
-    } finally {
-
+        log('error', 'exception-caught', {
+            stack: err,
+            path: request.url,
+            method: request.method,
+            ip: request.ip,
+            ua: request.headers['user-agent'] || null
+        })
+        reply.status(500).send('Error registering user.')
     }
 }
